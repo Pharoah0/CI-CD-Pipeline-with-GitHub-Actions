a0298479ea9dedd34288571a9ad678c9c0ad625a
Pharoah Evelyn <Pharoah@Pharoahs-Mac-Studio.local>
2025-02-16 00:16:11 -0500

    code edit

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -5,7 +5,11 @@ AWS_REGION="us-east-1"
 LAMBDA_NAME="EkimetricsDataProcessing"
 
 # Package Lambda
-zip function.zip lambda_function.py
+rm -rf package
+mkdir package
+pip install boto3 pandas numpy -t package/
+cp lambda_function.py package/
+cd package && zip -r ../function.zip .
 
 # Deploy to AWS Lambda
 aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://function.zip --region $AWS_REGION
